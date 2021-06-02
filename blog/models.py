8990b06ca0ac1a594d54be02ebc53c7be739cdf7
adilb <Adilbek-Akhmet>
2021-06-02 08:32:18 +0600

    commit

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     text = models.TextField()
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(default=timezone.now)
 
     def get_absolute_url(self):
         return reverse("blogs:blog_detail", kwargs={"pk": self.pk})
